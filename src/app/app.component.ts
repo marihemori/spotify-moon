90ec4d9f66fc06a13fd7110531389243a576b7f7
Mariana <moraismariana83@gmail.com>
2023-09-27 02:00:40 -0300

    Fix route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SpotifyService } from './services/spotify.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularSpotifyMoonMood';
+
+  constructor(private spotifyService: SpotifyService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.getTokenUrlCallback();
+  }
+
+  getTokenUrlCallback() {
+    const token = this.spotifyService.getTokenUrlCallback();
+
+    if (!!token) {
+      this.spotifyService.defineAccessToken(token);
+      this.router.navigate(['/player/home']);
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  openLoginPage() {
+    window.location.href = this.spotifyService.getUrlLogin();
+  }
 }
